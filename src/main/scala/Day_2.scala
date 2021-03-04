b37922efbf1b819d5ab2bb7b8a7791fea7e35d1b
Mendrejk <bastian.lakomy@gmail.com>
2021-03-04 17:09:42 +0100

    Finished Day 2.

--- src/main/scala/Day_2.scala
+++ src/main/scala/Day_2.scala
@@ -1,30 +1,53 @@
 import IoUtilities.readLinesToListString
 
 case class Password
-  (occurrencesMin: Int, occurrencesMax: Int, symbol: Char, password: String)
+  (occurrencesMin: Int, occurrencesMax: Int, symbol: Char, pass: String)
 
 object Day_2 {
   def main(args: Array[String]): Unit = {
     val directory: String = "resources/DataDay_2.txt"
     val data: Option[List[String]] = readLinesToListString(directory)
-
-    val validatedCount: Option[Int] = data match {
-      case Some(passwordStrings: List[String]) =>
-        Some(passwordStrings.count { (passwordString: String) =>
-          parsePassword(parseToList(passwordString)) match {
-            case Some(pass: Password) =>
-              validatePassword(pass)
-            case None => false
-          }
-        })
+    val passwords: Option[List[Password]] = data match {
+      case Some(passStrings: List[String]) => Some(parsePasswords(passStrings))
       case None => None
     }
 
-    validatedCount match {
-      case Some(count: Int) => print(validatedCount)
-      case None =>
+    val validatedByOccurrenceCount: Option[Int] = passwords match {
+      case Some(passwords: List[Password]) =>
+        Some(countValidPasswordsByOccurrence(passwords))
+      case None => None
+    }
+
+    val validatedByPositionCount: Option[Int] = passwords match {
+      case Some(passwords: List[Password]) =>
+        Some(countValidPasswordsByPosition(passwords))
+      case None => None
+    }
+
+    (validatedByOccurrenceCount, validatedByPositionCount) match {
+      case (Some(countByOccurrences: Int), Some(countByPosition: Int))
+        => print(s"by occurences: $countByOccurrences" +
+                 s", by position: $countByPosition")
+      case _ =>
         println("Data reading error... Please check the given directory.")
     }
+  }
+
+  def countValidPasswordsByOccurrence(passwords: List[Password]): Int = {
+    countValidPasswordsBy(passwords) { (pass: Password) =>
+      validatePasswordByOccurrences(pass)
+    }
+  }
+
+  def countValidPasswordsByPosition(passwords: List[Password]): Int = {
+    countValidPasswordsBy(passwords) { (pass: Password) =>
+      validatePasswordByPosition(pass)
+    }
+  }
+
+  private def countValidPasswordsBy
+    (passwords: List[Password]) (predicate: Password => Boolean): Int = {
+      passwords.count { (pass: Password) => predicate(pass) }
   }
 
   def parseToList(passwordString: String): List[String] =
@@ -52,11 +75,30 @@ object Day_2 {
     }
   }
 
-  def validatePassword(password: Password): Boolean = {
-    password.occurrencesMin to password.occurrencesMax contains
-    password.password.count((char: Char) => char == password.symbol)
+  def parsePasswords(passwordStrings: List[String]): List[Password] = {
+    passwordStrings
+      // flatten discards "None" Passwords
+      .flatMap((passString: String) => parsePassword(parseToList(passString)))
   }
 
+  def validatePasswordByOccurrences(password: Password): Boolean = {
+    password.occurrencesMin to password.occurrencesMax contains
+    password.pass.count((char: Char) => char == password.symbol)
+  }
+
+  def validatePasswordByPosition(password: Password): Boolean = {
+    password match {
+      case Password(low, high, char, pass) =>
+        // XOR operator
+        // counting from 1 rather than 0
+        pass.charAt(low - 1) == char ^ pass.charAt(high - 1) == char
+        // same as
+//        val lowTrue: Boolean = pass.charAt(low - 1) == char
+//        val highTrue: Boolean = pass.charAt(high - 1) == char
+//        (lowTrue && !highTrue) || (highTrue && !lowTrue)
+      case _ => false
+    }
+  }
 }
 
 
